267a044fcdeefcd2527e56eeaaa6f08b2e397f17
Alucard <314662958@qq.com>
2020-12-06 16:34:39 +0800

    fix some bugs.

--- play.js
+++ play.js
@@ -1,62 +1,63 @@
-function playSound(e, obj){
+
+let soundObj = document.createElement("audio");
+
+function playSound(e){
     if(e.code == "KeyA")
     {
         console.log("Key 'A' is pressed");
-        obj = document.getElementById("aSound");
+        soundObj = document.getElementById("aSound");
     }
     else if (e.code == "KeyS"){
         console.log("Key 'S' is pressed");
-        obj =    document.getElementById("sSound");
+        soundObj = document.getElementById("sSound");
     }else if (e.code == "KeyD"){
         console.log("Key 'D' is pressed");
-        obj = document.getElementById("dSound");
+        soundObj = document.getElementById("dSound");
     }else if (e.code == "KeyF"){
         console.log("Key 'F' is pressed");
-        obj = document.getElementById("fSound");
+        soundObj = document.getElementById("fSound");
 
     }else if (e.code == "KeyG"){
         console.log("Key 'G' is pressed");
-        obj = document.getElementById("gSound");
+        soundObj = document.getElementById("gSound");
     }else if (e.code == "KeyH"){
         console.log("Key 'H' is pressed");
-        obj = document.getElementById("hSound");
+        soundObj = document.getElementById("hSound");
     }else if (e.code == "KeyJ"){
         console.log("Key 'J' is pressed");
-        obj = document.getElementById("jSound");
+        soundObj = document.getElementById("jSound");
     }else if(e.code == "KeyE"){
         console.log("Key 'E' is pressed");
-        obj = document.getElementById("eSound");
+        soundObj = document.getElementById("eSound");
     }else if(e.code == "KeyT"){
         console.log("Key 'T' is pressed");
-        obj = document.getElementById("tSound");
+        soundObj = document.getElementById("tSound");
     }else if(e.code == "KeyU"){
         console.log("Key 'U' is pressed");
-        obj = document.getElementById("uSound");
+        soundObj = document.getElementById("uSound");
     }else if(e.code == "KeyW"){
         console.log("Key 'W' is pressed");
-        obj = document.getElementById("wSound");
+        soundObj = document.getElementById("wSound");
     }else if(e.code == "KeyY"){
         console.log("Key 'Y' is pressed");
-        obj = document.getElementById("ySound");
+        soundObj = document.getElementById("ySound");
     }
-    obj.play();
+    soundObj.load();
+    soundObj.play();
 }
 
 document.addEventListener("keydown",function (e){
-    let soundObj = document.createElement("audio");
-    playSound(e, soundObj);
+    playSound(e);
     console.log(e.key);
     let tmpkey = document.getElementById(e.key);
     tmpkey.style.background = "gray";
 });
 
 document.addEventListener("keyup", function (e){
-
     let element = document.getElementById(e.key);
     if(element.parentElement.className == "white-keys"){
         element.style.background = "white";
     }else{
         element.style.background = "black";
     }
-
 });
